81eafeec64878e341443f6a331aa744bd6cdb12b
SaeedDev1 <magsisaeed3@gmail.com>
2024-03-30 17:22:00 -0700

    New Test Component Added

---
+++ components/landing-page-sections/test-component.js
@@ -0,0 +1,33 @@
+import parse from 'html-react-parser';
+
+export default function testComponent({ header, copy, icons = []}) {
+	return (
+        <section className="cta-section py-5">
+			<div className="container">
+				<div className="testrow">
+					<div className="testcol">
+						<div className="cta-content-wrapper">
+							<div className="section-title">
+                                <div className='row align-items-center'>
+                                    <div className='col-sm-12 col-lg-6'>
+                                        <h2 className="mb-20">{header}</h2>
+                                        {parse(copy)}
+                                    </div>
+                                    <div className='col-sm-12 col-lg-6 row'>
+                                        {icons.map((iconS, index)=>{
+                                            return(
+                                                <div className='col-lg-4' key={index}>
+                                                    <img src={iconS.icon} alt="Test icon"></img>
+                                                </div>
+                                            );
+                                        })}
+                                    </div>
+                                </div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section >
+    );
+}
