bbd94c02109bffb9dec1b283b346f5627dce80b0
S2005-qatar <125984910+S2005-qatar@users.noreply.github.com>
2024-05-12 09:36:36 +0300

    Update [slug].js

--- pages/landing-page/[slug].js
+++ pages/landing-page/[slug].js
@@ -30,6 +30,19 @@ export default function LandingPage({ page, blogPosts }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="shortcut icon" type="image/x-icon" href="https://buttercms.com/static/v2/images/favicon.png" />
       </Head>
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/66405d0207f59932ab3e9c9a/1htlmml37';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
 
       {page.fields.body.map(({ type, fields: sectionData }, index) =>
         <LandingPageSection
