ff392745845e7023000194d4d016f1fd954a7067
S2005-qatar <125984910+S2005-qatar@users.noreply.github.com>
2024-05-12 09:32:50 +0300

    Update footer-section.js

--- components/footer-section.js
+++ components/footer-section.js
@@ -31,6 +31,19 @@ export default function FooterSection({ mainMenu }) {
                                 <li><a href="#0"><i className="lni lni-linkedin"></i></a></li>
                                 <li><a href="#0"><i className="lni lni-instagram"></i></a></li>
                                 <li><a href="#0"><i className="lni lni-twitter"></i></a></li>
+                                <!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/66405d0207f59932ab3e9c9a/1htlmml37';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
                             </ul>
                         </div>
                     </div>
